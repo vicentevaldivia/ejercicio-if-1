fac84198bd1d492f117c2bc48c8db0ac868e11da
Vicente Valdivia <vicentevaldivia21@gmail.com>
2023-05-20 22:05:16 -0400

    Ejercicio if 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String estacion = "primavera";
+
+        if (estacion.equals("primavera")) {
+            System.out.println("Es primavera");
+        } else if (estacion.equals("verano")) {
+            System.out.println("Es verano");
+        } else {
+            System.out.println("Es_otra_estacion");
+        }
+    }
+}
